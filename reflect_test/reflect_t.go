80ba9fb18cdd329a6ba77e726b0ac0f4bbd0a9c2
Pharsalia <xpdh@outlook.com>
2022-04-09 01:44:49 +0800

    test

--- reflect_test/reflect_t.go
+++ reflect_test/reflect_t.go
@@ -1,1 +1,32 @@
-package reflect_test
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func reflectType(x interface{}) {
+	type_ := reflect.TypeOf(x)
+	fmt.Printf("Type:%v Name:%v kind:%v\n", type_, type_.Name(), type_.Kind())
+
+}
+
+type Animal interface {
+	bar()
+}
+
+type cat struct {
+	name string
+}
+
+func (c cat) bar() {
+	fmt.Printf("my name is %s\n", c.name)
+}
+
+func main() {
+	cat := cat{name: "绯鞠"}
+	cat.bar()
+	reflectType(cat)
+	var t1 []int
+	reflectType(t1)
+}
