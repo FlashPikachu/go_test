32d49361d1ec528c34caaae48134c74d1a3250b5
Pharsalia <942572671@qq.com>
2022-06-05 21:04:43 +0800

    save

--- othertest/test_1/test_1.go
+++ othertest/test_1/test_1.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
+type User struct {
+	name string
+	age  int
+}
+
+func (u User) sayName() {
+	fmt.Printf("my name is %v\n", u.name)
+}
+
 func main() {
 	if a := 1; false {
 	} else if b := 2; false {
@@ -7,4 +18,11 @@ func main() {
 	} else {
 		println(a, b, c)
 	}
+
+	user1 := User{
+		name: "pharsalia",
+		age:  10,
+	}
+
+	user1.sayName()
 }
