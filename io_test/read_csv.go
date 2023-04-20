c0d3f495bdef48422eb8967ef136a603a0692869
Dihao <xpdh@outlook.com>
2023-04-20 22:01:39 +0800

    redis test

---
+++ io_test/read_csv.go
@@ -0,0 +1,19 @@
+package main
+
+type product struct {
+	title string
+	price float32
+	count int
+}
+
+//func main() {
+//	file, err := os.Open("io_test/input.txt")
+//	if err != nil {
+//		fmt.Println("read file error!")
+//	}
+//
+//}
+//
+//func readLine(line string) *product {
+//	f
+//}
---
+++ redis_t/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
+
+var rdb *redis.Client
+
+func main() {
+	ctx := context.Background()
+	err := rdb.Set(ctx, "goredistestkey", "goredistestvalue", 0).Err()
+	if err != nil {
+		panic(any(err))
+	}
+	result, err := rdb.Get(ctx, "goredistestkey").Result()
+	if err != nil {
+		panic(any(err))
+	}
+	println("what the fkk")
+	println(result)
+
+	ret, err := rdb.Do(ctx, "get", "goredistestkey").Result()
+	if err != nil {
+		panic(any(err))
+	}
+	fmt.Println(ret.(string))
+}
+
+func init() {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "47.110.240.147:6379",
+		Password: "valhalla", // 没有密码，默认值
+		DB:       0,          // 默认DB 0
+	})
+}
